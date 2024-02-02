3e5d7ca50090b66a2adc6392db493075f24de7a2
Nick <nicksweetj@gmail.com>
2024-02-01 21:35:25 -0500

    added login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,102 @@
-import React from 'react'
+import React, { useState } from "react";
+import * as Form from "@radix-ui/react-form";
+import Layout from "../components/Layout";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [emailFocus, setEmailFocus] = useState(false);
+  const [usernameFocus, setUsernameFocus] = useState(false);
+  const [passwordFocus, setPasswordFocus] = useState(false);
 
-export default Login
+  return (
+    <Layout>
+      <div className="flex items-center justify-center h-screen bg-gradient-to-r from-background to-secondary">
+        <div className="text-center border border-three p-8 bg-four rounded-md shadow-md">
+          <h2 className="text-2xl font-semibold mb-6 text-seven">Sign Up</h2>
+          <Form.Root method="post">
+            <Form.Field name="email">
+              <div
+                className='mb-5'
+              >
+                <Form.Label
+                  className={`block text-left mb-2 ${
+                    emailFocus ? "text-one" : "text-six"
+                  }`}
+                >
+                  Email
+                </Form.Label>
+                <Form.Control
+                  className={`w-full p-2 border rounded-md focus:outline-none ${
+                    emailFocus ? "focus:border-primary" : ""
+                  }`}
+                  onFocus={() => setEmailFocus(true)}
+                  onBlur={() => setEmailFocus(false)}
+                />
+                <Form.Message className="text-red-500" match="valueMissing">
+                  Please enter your Email
+                </Form.Message>
+              </div>
+            </Form.Field>
+
+            <Form.Field name="username">
+              <div
+                className='mb-5'
+              >
+                <Form.Label
+                  className={`block text-left mb-2 ${
+                    usernameFocus ? "text-one" : "text-six"
+                  }`}
+                >
+                  Username
+                </Form.Label>
+                <Form.Control
+                  className={`w-full p-2 border rounded-md focus:outline-none ${
+                    usernameFocus ? "focus:border-primary" : ""
+                  }`}
+                  onFocus={() => setUsernameFocus(true)}
+                  onBlur={() => setUsernameFocus(false)}
+                />
+                <Form.Message className="text-red-500" match="valueMissing">
+                  Please enter a username
+                </Form.Message>
+              </div>
+            </Form.Field>
+
+            <Form.Field name="password">
+              <div
+                className='mb-4'
+              >
+                <Form.Label
+                  className={`block text-left mb-2 ${
+                    passwordFocus ? "text-one" : "text-six"
+                  }`}
+                >
+                  Password
+                </Form.Label>
+                <Form.Control
+                  className={`w-full p-2 border rounded-md focus:outline-none ${
+                    passwordFocus ? "focus:border-primary" : ""
+                  }`}
+                  onFocus={() => setPasswordFocus(true)}
+                  onBlur={() => setPasswordFocus(false)}
+                  autoComplete="new-password"
+                  type="password"
+                />
+                <Form.Message className="text-red-500" match="valueMissing">
+                  Please enter a password
+                </Form.Message>
+              </div>
+            </Form.Field>
+
+            <Form.Submit>
+              <button className="w-full bg-seven text-five p-2 rounded-lg hover:text-one hover:bg-background focus:outline-none">
+                Sign Up
+              </button>
+            </Form.Submit>
+          </Form.Root>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Login;
